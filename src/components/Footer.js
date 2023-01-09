2d4e43b8b95b368ad2f14937e69b1af071144f71
VenkateshKrishna <venkateshkrishna997@gmail.com>
2023-01-09 19:15:12 +0530

    updated links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,10 @@
 import React from "react";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="socialMedia">
-        <LinkedInIcon />
-      </div>
-      <p> &copy; 2022 venkateshkrishna.com </p>
+      <p> &copy; 2022-23 venkateshkrishna.com </p>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,20 +11,26 @@ function Home() {
         <h2> Hi, My name is Venkatesh</h2>
         <div className="prompt">
           <p>A Software Developer with a passion for learning and creating.</p>
-          <LinkedInIcon />
-          <EmailIcon />
-          <GithubIcon />
+          <a href="https://www.linkedin.com/in/venkatesh-krishna-093956b9/">
+            <LinkedInIcon />
+          </a>
+          <a href="mailto:venkateshkrishnaashok@gmail.com">
+            <EmailIcon />
+          </a>
+          <a href="https://github.com/venkasa">
+            <GithubIcon />
+          </a>
         </div>
       </div>
       <div className="skills">
         <h1>skills</h1>
         <ol className="list">
           <li className="item">
-            <h2> Front-End:</h2>
+            <h2>Front-End:</h2>
             <h5>HTML,CSS,JavaScript,ReactJS,Redux and MaterialUI</h5>
           </li>
           <li className="item">
-            <h2> Design:</h2>
+            <h2>Design:</h2>
             <h5>Figma</h5>
           </li>
         </ol>
